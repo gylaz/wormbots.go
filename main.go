ee91fc390a11d552934addbd6d802c862ba30ab1
Greg Lazarev <greg@thoughtbot.com>
2016-09-08 18:42:29 -0700

    Simulate farm in main loop

--- main.go
+++ main.go
@@ -9,6 +9,25 @@ import (
 	"github.com/gylaz/wormbots.go/farm"
 )
 
+func main() {
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("public"))
+	f := farm.New()
+
+	go func() {
+		for {
+			time.Sleep(time.Second / 15)
+
+			f.Tick()
+		}
+	}()
+
+	mux.Handle("/", fs)
+	mux.HandleFunc("/world", streamData(&f))
+
+	http.ListenAndServe(":4000", mux)
+}
+
 func streamData(f *farm.Farm) func(http.ResponseWriter, *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
 		res.Header().Set("Content-type", "text/event-stream")
@@ -16,27 +35,10 @@ func streamData(f *farm.Farm) func(http.ResponseWriter, *http.Request) {
 		res.Header().Set("Connection", "keep-alive")
 
 		for {
-			fmt.Fprintf(res, "data: %v\n\n", data(f))
+			worms, _ := json.Marshal(f.Worms)
+			fmt.Fprintf(res, "data: %v\n\n", string(worms))
 			res.(http.Flusher).Flush()
 			time.Sleep(time.Second / 15)
-			f.Tick()
 		}
 	}
-}
-
-func main() {
-	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("public"))
-	f := farm.New()
-
-	mux.Handle("/", fs)
-	mux.HandleFunc("/world", streamData(&f))
-
-	http.ListenAndServe(":3000", mux)
-}
-
-func data(f *farm.Farm) string {
-	data, _ := json.Marshal(f.Worms)
-
-	return string(data)
 }
