7747f0ff020b57f4c92e3de471eb8d5514a289c1
Greg Lazarev <greg@thoughtbot.com>
2016-09-08 19:31:41 -0700

    Replace moving tests with truth table

--- worm/worm_test.go
+++ worm/worm_test.go
@@ -6,114 +6,34 @@ import (
 	"github.com/gylaz/wormbots.go/worm"
 )
 
-func TestMovingUpIntoTopRightCorner(t *testing.T) {
-	w := worm.Worm{Dir: "up", X: worm.MaxX, Y: worm.MinY}
-
-	w.Move()
-
-	if w.Dir != "left" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MaxX-1 || w.Y != worm.MinY {
-		t.Error("Worm did not move")
-	}
+var scenarios = []struct {
+	worm        worm.Worm
+	expectedDir string
+	expectedX   int
+	expectedY   int
+}{
+	{worm.Worm{"up", worm.MaxX, worm.MinY}, "left", worm.MaxX - 1, worm.MinY},
+	{worm.Worm{"up", worm.MinX, worm.MinY}, "right", worm.MinX + 1, worm.MinY},
+	{worm.Worm{"down", worm.MaxX, worm.MaxY}, "left", worm.MaxX - 1, worm.MaxY},
+	{worm.Worm{"down", worm.MinX, worm.MaxY}, "right", worm.MinY + 1, worm.MaxY},
+	{worm.Worm{"right", worm.MaxX, worm.MinY}, "down", worm.MaxX, worm.MinY + 1},
+	{worm.Worm{"right", worm.MaxX, worm.MaxY}, "up", worm.MaxX, worm.MaxY - 1},
+	{worm.Worm{"left", worm.MinX, worm.MinY}, "down", worm.MinX, worm.MinY + 1},
+	{worm.Worm{"left", worm.MinX, worm.MaxY}, "up", worm.MinX, worm.MaxY - 1},
 }
 
-func TestMovingUpIntoTopLeftCorner(t *testing.T) {
-	w := worm.Worm{Dir: "up", X: worm.MinX, Y: worm.MinY}
+func TestMove(t *testing.T) {
+	for _, scenario := range scenarios {
+		var worm = scenario.worm
 
-	w.Move()
+		worm.Move()
 
-	if w.Dir != "right" {
-		t.Error("Worm did not turn")
-	}
+		if worm.Dir != scenario.expectedDir {
+			t.Error("Worm did not turn")
+		}
 
-	if w.X != worm.MinX+1 || w.Y != worm.MinY {
-		t.Error("Worm did not move")
-	}
-}
-
-func TestMovingDownIntoBottomRightCorner(t *testing.T) {
-	w := worm.Worm{Dir: "down", X: worm.MaxX, Y: worm.MaxY}
-
-	w.Move()
-
-	if w.Dir != "left" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MaxX-1 || w.Y != worm.MaxY {
-		t.Error("Worm did not move")
-	}
-}
-
-func TestMovingDownIntoBottomLeftCorner(t *testing.T) {
-	w := worm.Worm{Dir: "down", X: worm.MinX, Y: worm.MaxY}
-
-	w.Move()
-
-	if w.Dir != "right" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MinX+1 || w.Y != worm.MaxY {
-		t.Error("Worm did not move")
-	}
-}
-
-func TestWormMovingRightIntoTopRightCorner(t *testing.T) {
-	w := worm.Worm{Dir: "right", X: worm.MaxX, Y: worm.MinY}
-
-	w.Move()
-
-	if w.Dir != "down" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MaxX || w.Y != worm.MinY+1 {
-		t.Error("Worm did not move")
-	}
-}
-
-func TestMovingRightIntoBottomRightCorner(t *testing.T) {
-	w := worm.Worm{Dir: "right", X: worm.MaxX, Y: worm.MaxY}
-
-	w.Move()
-
-	if w.Dir != "up" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MaxX || w.Y != worm.MaxY-1 {
-		t.Error("Worm did not move")
-	}
-}
-
-func TestWormMovingLeftIntoTopLeftCorner(t *testing.T) {
-	w := worm.Worm{Dir: "left", X: worm.MinX, Y: worm.MinY}
-
-	w.Move()
-
-	if w.Dir != "down" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MinX || w.Y != worm.MinY+1 {
-		t.Error("Worm did not move")
-	}
-}
-
-func TestMovingLeftIntoBottomLeftCorner(t *testing.T) {
-	w := worm.Worm{Dir: "left", X: worm.MinX, Y: worm.MaxY}
-
-	w.Move()
-
-	if w.Dir != "up" {
-		t.Error("Worm did not turn")
-	}
-
-	if w.X != worm.MinX || w.Y != worm.MaxY-1 {
-		t.Error("Worm did not move")
+		if worm.X != scenario.expectedX || worm.Y != scenario.expectedY {
+			t.Error("Worm did not move")
+		}
 	}
 }
